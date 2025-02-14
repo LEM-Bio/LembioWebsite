3e476ed3cbdf5c3c006cebeddf3928eef491fb2d
a <a@>
2025-02-14 14:38:10 -0300

    mudanças nos coordenadores e na equipe

--- src/routes/Equipe.js
+++ src/routes/Equipe.js
@@ -3,6 +3,26 @@ import '../styles/coordenadores.css'
 function Equipe(){
     return(
         <div>
+            <h2 className='categoriaTitle'>Docentes</h2>
+            <div  className="Corpo">
+                <div className="coord">
+                    <h5>Prof. Dr. Marcos Sergio de Toledo</h5>
+                    <a href='http://lattes.cnpq.br/3308810633774571' target="_blank" rel="noreferrer">
+                        <p>Currículo Lattes</p>
+                    </a>
+                    <p>Departamento de Bioquímica</p>
+                    <p>Escola Paulista de Medicina/UNIFESP</p>
+                </div>
+                <div className="coord">
+                    <h5>Prof. Dr. André Zelanis</h5>
+                    <a href='http://lattes.cnpq.br/8149569348608834' target="_blank" rel="noreferrer">
+                        <p>Currículo Lattes</p>
+                    </a>
+                    <p>Professor Adjunto</p>
+                    <p>ICT/UNIFESP</p>
+                </div>
+            </div>
+            <br></br>
             <h2 className='categoriaTitle'>Equipe Técnica</h2>
             <div  className="Corpo">
                 <div className="coord">
@@ -22,6 +42,8 @@ function Equipe(){
                     <p>Escola Paulista de Medicina/UNIFESP</p>
                 </div>
             </div>
+            <br></br>
+            <h2 className='categoriaTitle'>Estudantes</h2>
         </div>
     )
 }
