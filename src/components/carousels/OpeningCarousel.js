140c2380a2bb72b0f12bc394ba03579b0e30c2f1
a <a@>
2025-05-14 17:21:27 -0300

    Passagem do carousel para o json

--- src/components/carousels/OpeningCarousel.js
+++ src/components/carousels/OpeningCarousel.js
@@ -2,60 +2,21 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Carousel  from 'react-bootstrap/Carousel';
 import './opCarousel.css'
-
-import carousel1 from '../../img/home/slider/carousel1.jpeg'
-import carousel2 from '../../img/home/slider/carousel2.jpeg'
-import carousel3 from '../../img/home/slider/carousel3.jpg'
-import carousel4 from '../../img/home/slider/carousel4.png'
-import carousel5 from '../../img/home/slider/carousel5.jpeg'
-import carousel6 from '../../img/home/slider/carousel6.jpeg'
+import data from '../../data/components-mock.json'
 
 function OpeningCarousel() {
   return (
     <div className='carouselteste' style={{display: 'block', marginTop:70, marginBottom:100}}>
       <Carousel>
-        <Carousel.Item interval={3000}>
-          <img
-            className='imgCarousel'
-            src={carousel1}
-            alt="Lab"
-          />
-        </Carousel.Item>
-        <Carousel.Item interval={3000}>
-          <img
-            className='imgCarousel'
-            src={carousel2}
-            alt="Lab"
-          />
-        </Carousel.Item>
-        <Carousel.Item interval={3000}>
-          <img
-            className='imgCarousel'
-            src={carousel3}
-            alt="Lab"
-          />
-        </Carousel.Item>
-        <Carousel.Item interval={3000}>
-          <img
-            className='imgCarousel'
-            src={carousel4}
-            alt="Lab"
-          />
-        </Carousel.Item>
-        <Carousel.Item interval={3000}>
-          <img
-            className='imgCarousel'
-            src={carousel6}
-            alt="Lab"
-          />
-        </Carousel.Item>
-        <Carousel.Item interval={3000}>
-          <img
-            className='imgCarousel'
-            src={carousel5}
-            alt="Lab"
-          />
-        </Carousel.Item>
+        {data.carousel.map((item, index) => (
+                        <Carousel.Item interval={3000}>
+                          <img
+                            className='imgCarousel'
+                            src={item.image.url}
+                            alt={item.image.alt}
+                          />
+                        </Carousel.Item>
+                  ))}
       </Carousel>
     </div>
   );
